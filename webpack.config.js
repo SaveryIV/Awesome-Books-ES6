2edec2d970e835101d74218038d885685f19e8f2
Mona Lisa <soriaagustin8@gmail.com>
2023-07-18 18:42:33 -0300

    Fixing linter

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.js',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
 };
